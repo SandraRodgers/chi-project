f5a495cb2fd77716ec05391eefdbc40d17553c40
Sandra Rodgers <SRODG7@GMAIL.COM>
2021-04-18 23:22:53 -0500

    fixed chi pages

--- pages/_adopt/_slug.vue
+++ pages/_adopt/_slug.vue
@@ -1,81 +1,79 @@
 <template>
-    <div class="bg-whitesmoke pt-28 lg:pt-40 pb-6 md:pb-8 lg:pb-16 flex flex-col items-center justify-center w-full">
-      <hr class="h-2 mt-10 w-11/12" style="background: linear-gradient(90deg, hsla(197, 84%, 47%, 1) 50%, hsla(55, 100%, 50%, 1) 100%);">
-        <div class="flex items-center justify-center flex-col bg-white w-11/12">
-          <h1 class="title flex justify-center w-11/12 md:w-11/12 lg:w-11/12 text-xl md:text-xl lg:text-3xl font-bold bg-white pt-4 md:pt-6 lg:pt-8 ">{{chihuahua.chihuahua}}</h1>
-          <div class="text-center py-4 w-8/12">
-            <h2 class="font-semibold" v-if="chihuahua.region === 'southwest'">
+    <div class="bg-whitesmoke pt-10 lg:pt-10 pb-6 md:pb-8 lg:pb-16 flex flex-col items-center justify-center w-full">
+      <hr class="h-2 mt-10 w-11/12 rounded-t-md" style="background: linear-gradient(90deg, hsla(197, 84%, 47%, 1) 50%, hsla(55, 100%, 50%, 1) 100%);">
+        <div class="flex items-center justify-center flex-col bg-white w-11/12 shadow-2xl">
+          <h1 class="title flex justify-center w-11/12 md:w-11/12 lg:w-11/12 text-2xl md:text-3xl lg:text-3xl font-bold bg-white py-4 md:pt-6 lg:pt-8 text-center">{{chihuahua.chihuahua}}</h1>
+          <div class="text-center text-sm lg:text-base py-4 w-10/12 lg:w-8/12">
+            <h2 class="font-normal lg:font-semibold" v-if="chihuahua.region === 'southwest'">
               This dog is being fostered in Dallas TX and must be adopted within the Dallas, North Central Texas or the southern Oklahoma area.
             </h2>
-            <h2 class="font-semibold" v-if="chihuahua.region === 'midwest'">
+            <h2 class="font-normal lg:font-semibold" v-if="chihuahua.region === 'midwest'">
               All dogs listed here are adoptable in the following Midwestern states (OH / KY/ IL / MI / WI/ MN/). We do transport! They are spread out throughout the Midwest in foster homes.
             </h2>
-            <h2 class="font-semibold" v-if="chihuahua.region === 'southeast'">
+            <h2 class="font-normal lg:font-semibold" v-if="chihuahua.region === 'southeast'">
               North Carolina, South Carolina, Virginia, Tennessee (Nashville Area), Alabama, Georgia and Maryland
             </h2>
           </div>
-          <div class="px-14">
+          <div class="px-1 lg:px-14 md:px-14">
             <div class="flex items-start  py-4 md:py-6 lg:py-8 bg-white text-xs md:text-sm lg:text-base">
-              <div class="flex flex-col px-2 w-4/6">
+              <div class="flex flex-col px-2 w-5/6 lg:w-4/6">
               <div class="flex justify-center py-4">
-              <div class="w-5/6">
+              <div class="w-6/6">
               <div class="flex py-1">
-                <div class="w-28 font-medium">Name:</div>
-                <div class="ml-10">{{chihuahua.chihuahua}}</div>
+                <div class="w-6/12 sm:w-6/12 lg:w-28 md:w-18 font-medium">Name:</div>
+                <div class="w-6/12 sm:w-6/1 ml-2 md:ml-10 lg:ml-10">{{chihuahua.chihuahua}}</div>
               </div>
               <div class="flex py-1">
-                <div class="w-28 font-medium">Gender:</div>
-                <div class="ml-10">{{chihuahua.gender}}</div>
+                <div class="w-6/12 sm:w-6/12 lg:w-28 md:w-18 font-medium">Gender:</div>
+                <div class="w-6/12 sm:w-6/1 w-6/12 sm:w-6/1 ml-2 md:ml-10 lg:ml-10">{{chihuahua.gender}}</div>
               </div>
               <div class="flex py-1">
-                <div class="w-28 font-medium">Age:</div>
-                <div class="ml-10">{{chihuahua.age}}</div>
+                <div class="w-6/12 sm:w-6/12 lg:w-28 md:w-18 font-medium">Age:</div>
+                <div class="w-6/12 sm:w-6/1 ml-2 md:ml-10 lg:ml-10">{{chihuahua.age}}</div>
               </div>
               <div class="flex py-1">
-                <div class="w-28 font-medium">Weight:</div>
-                <div class="ml-10">{{chihuahua.weight}}</div>
+                <div class="w-6/12 sm:w-6/12 lg:w-28 md:w-18 font-medium">Weight:</div>
+                <div class="w-6/12 sm:w-6/1 ml-2 md:ml-10 lg:ml-10">{{chihuahua.weight}}</div>
               </div>
               <div class="flex py-1">
-                <div class="w-28 font-medium">Dogs/Cats:</div>
-                <div class="ml-10">{{chihuahua.dogsCats}}</div>
+                <div class="w-6/12 sm:w-6/12 lg:w-28 md:w-18 font-medium">Dogs/Cats:</div>
+                <div class="w-6/12 sm:w-6/1 ml-2 md:ml-10 lg:ml-10">{{chihuahua.dogsCats}}</div>
               </div>
               <div class="flex py-1">
-                <div class="w-28 font-medium">Children:</div>
-                <div class="ml-10">{{chihuahua.children}}</div>
+                <div class="w-6/12 sm:w-6/12 lg:w-28 md:w-18 font-medium">Children:</div>
+                <div class="w-6/12 sm:w-6/1 ml-2 md:ml-10 lg:ml-10">{{chihuahua.children}}</div>
               </div>
             </div>
             </div>
                 <block-content :blocks="child" v-for="child in chihuahua.description" :key="child._id" />
-                <div class="flex justify-center">
-                  <nuxt-link :to="{ path: `/adopt` }" tag="button" class="bg-middleYellow text-sm md:text-sm lg:text-lg p-1 md:p-2 lg:p-3 mt-8 md:mt-6 lg:mt-10 w-3/5 md:w-2/5 lg:w-2/5 text-black hover:opacity-70 rounded-sm">Adoption Application</nuxt-link>
-                </div>
               </div>
               <div class="flex flex-col ml-2 items-end w-2/6">
                 <div v-if="chihuahua.mainImage" class="px-2 py-1">
-                  <img class="h-80" :src="imageUrlFor(chihuahua.mainImage)"/>
+                  <img class="h-20 lg:h-80 sm:h-20" :src="imageUrlFor(chihuahua.mainImage)"/>
                 </div>
                 <div v-if="chihuahua.imageTwo" class="px-2 py-1">
-                  <img class="h-80" :src="imageUrlFor(chihuahua.imageTwo)"/>
+                  <img class="h-20 lg:h-80 sm:h-20" :src="imageUrlFor(chihuahua.imageTwo)"/>
                 </div>
                 <div v-if="chihuahua.imageThree" class="px-2 py-1">
-                  <img class="h-80" :src="imageUrlFor(chihuahua.imageThree)"/>
+                  <img class="h-20 lg:h-80 sm:h-20" :src="imageUrlFor(chihuahua.imageThree)"/>
                 </div>
                 <div v-if="chihuahua.imageFour" class="px-2 py-1">
-                  <img class="h-80" :src="imageUrlFor(chihuahua.imageFour)"/>
+                  <img class="h-20 lg:h-80 sm:h-20" :src="imageUrlFor(chihuahua.imageFour)"/>
                 </div>
                 <div v-if="chihuahua.imageFive" class="px-2 py-1">
-                  <img class="h-80" :src="imageUrlFor(chihuahua.imageFive)"/>
+                  <img class="h-20 lg:h-80 sm:h-20" :src="imageUrlFor(chihuahua.imageFive)"/>
                 </div>
                 <div v-if="chihuahua.imageSix" class="px-2 py-1">
-                  <img class="h-80" :src="imageUrlFor(chihuahua.imageSix)"/>
+                  <img class="h-20 lg:h-80 sm:h-20" :src="imageUrlFor(chihuahua.imageSix)"/>
                 </div>
-                
-                
               </div>
               </div>
             </div>
+            <div class="flex justify-center">
+                  <nuxt-link :to="{ path: `/adopt` }" tag="button" class="bg-middleYellow text-sm md:text-sm lg:text-lg p-2 md:p-2 lg:p-3 my-2 md:my-6 lg:my-10 w-5/5 md:w-5/5 lg:w-5/5 text-black hover:opacity-70 rounded-sm">Adoption Application</nuxt-link>
+                </div>
           </div>
-        <hr class="h-2 w-11/12" style="background: linear-gradient(90deg, hsla(197, 84%, 47%, 1) 50%, hsla(55, 100%, 50%, 1) 100%);">
+        <hr class="h-2 w-11/12 rounded-b-md" style="background: linear-gradient(90deg, hsla(197, 84%, 47%, 1) 50%, hsla(55, 100%, 50%, 1) 100%);">
     </div>
 </template>
 
